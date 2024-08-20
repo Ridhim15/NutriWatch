8e14f9a98fe64b00cd5a259c8f404ea1ec191db4
Ridhim15 <ridhimg15@gmail.com>
2024-08-20 20:11:05 +0530

    okay now !!

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
-from wsgi import app
+from flask import Flask, render_template, redirect, url_for, session, request, jsonify, make_response, send_from_directory
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
