52d513e2731ae856abbf437420613e7af8efb321
Ridhim15 <ridhimg15@gmail.com>
2024-08-28 13:52:14 +0530

    port updated

--- app.py
+++ app.py
@@ -69,8 +69,8 @@ if __name__ == '__main__':
     if not os.path.exists("uploads"):
         os.makedirs("uploads")
     # app.run(debug=True,host='192.168.29.235')
-    app.run(debug=True)
+    # app.run(debug=True)
     port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
